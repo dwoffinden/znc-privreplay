6d53509dd3a05e3fc8ec10d4acdf83f4029896c4
John Schoenick <jschoenick@mozilla.com>
2012-01-06 13:29:12 -0600

    [<-] Should be for outgoing and [->] for incoming

--- privreplay.cpp
+++ privreplay.cpp
@@ -16,7 +16,7 @@ public:
 
     virtual EModRet OnPrivMsg(CNick& Nick, CString& sMessage)
     {
-        CString msg = CString("[<-] ") + sMessage;
+        CString msg = CString("[->] ") + sMessage;
         StoreMessage(Nick, msg);
 
         return (CONTINUE);
@@ -50,7 +50,7 @@ public:
                 msg += " " + vsRet[c];
             }
 
-            StoreMessage(":" + vsRet[1] + " PRIVMSG " + m_pUser->GetIRCNick().GetNick() + " :[->] " + msg);
+            StoreMessage(":" + vsRet[1] + " PRIVMSG " + m_pUser->GetIRCNick().GetNick() + " :[<-] " + msg);
         }
         return (CONTINUE);
     }
